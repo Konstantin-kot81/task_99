e2bd3a428cfbc2375e39da22aaf38eda2f94aa63
Konstantin <konstantin.b81@gmail.com>
2022-02-14 11:31:01 +0200

    add new

--- p23_rnd_select.erl
+++ p23_rnd_select.erl
@@ -13,7 +13,8 @@
 -export([
          select/0,
          select/2,
-         select2/0
+         select2/0,
+         select2/2
         ]).
 
 %% ([a, b, c, d, e, f, g, h], 3) -> [e, d, a]
@@ -29,8 +30,27 @@ select(List, X) ->
 %% второй вариант решения
 
 select2() ->
-  select2(select()).
+  select2([a, b, c, d, e, f, g, h], 3).
 
+select2(List, X) ->
+  Range = lenght:lenght(List),
+  RandNums = generate_randoms(X, Range),
+get_elements(List, RandNums).
 
+generate_randoms(X, Range) ->
+  generate_randoms(X, Range, []).
 
-%%  RandNums = generate_random(X, Range),
+generate_randoms(0, _, Acc) -> Acc;
+
+generate_randoms(X, Range,Acc) ->
+  Random = rand:uniform(Range),
+generate_randoms(X - 1, Range, [Random | Acc]).
+
+get_elements(List, RandNums) ->
+  get_elements(List, RandNums, []).
+
+get_elements(List, [N | RandNum], Acc) ->
+  Element = p03_find_element:find_element(List, N),
+  get_elements(List, RandNum, [Element | Acc]);
+
+get_elements(_, [], Acc) -> Acc.
